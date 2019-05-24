ec89d1adb0050f19ab5e5c145aee02be66df7c8f
BrunoCata <mb_cataneo@hotmail.com>
2019-05-24 11:43:34 -0300

    Comitando alterações

--- gastos.php
+++ gastos.php
@@ -21,7 +21,7 @@
         $conecta->execute();
         $gastos = $conecta->fetchAll(PDO::FETCH_ASSOC);
       ?>  
-
+    
     <div class="row justify-content-center">
         <table class="table formulario">
           <thead>
@@ -58,7 +58,7 @@
 
 <div class="row justify-content-center">
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" cantesrossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
       <form action="processoGastos.php" method="POST">
         <input type="hidden" name="id_produto" value="<?=$id_produto?>">
